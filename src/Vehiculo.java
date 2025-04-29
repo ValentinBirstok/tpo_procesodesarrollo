b43d20d3b444aac90e9116aa124f999f409c1cbe
Valentin Birstok <valenbirstok22@gmail.com>
2025-04-29 19:24:39 -0300

    2do commit

---
+++ src/Vehiculo.java
@@ -0,0 +1,47 @@
+public class Vehiculo {
+    private String marca;
+    private String modelo;
+    private String color;
+    private String numeroChasis;
+    private String numeroMotor;
+
+    public Vehiculo(String marca, String modelo, String color, String numeroChasis, String numeroMotor) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.color = color;
+        this.numeroChasis = numeroChasis;
+        this.numeroMotor = numeroMotor;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getNumeroChasis() {
+        return numeroChasis;
+    }
+
+    public String getNumeroMotor() {
+        return numeroMotor;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehiculo{" +
+                "marca='" + marca + '\'' +
+                ", modelo='" + modelo + '\'' +
+                ", color='" + color + '\'' +
+                ", número de chasis='" + numeroChasis + '\'' +
+                ", número de motor='" + numeroMotor + '\'' +
+                '}';
+    }
+}
+
